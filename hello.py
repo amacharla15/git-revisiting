b09cf88ebe6d1acf46711af10aa5c90428b14d26
amacharla15 <amacharla@csuchico.edu>
2025-07-01 15:31:31 -0700

    commit message

---
+++ hello.py
@@ -0,0 +1,22 @@
+
+#Initialization
+"""
+git init -- Initializing the git repo
+
+ls -la -- .git
+
+cd ..
+
+"""
+# Commit Phase
+"""
+git status -- ran before commiting or running git add . , so I got untracked 
+
+to track:
+git add . --success
+git status - shows to be commited
+
+
+
+
+"""
